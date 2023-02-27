4f9ec83ccd1e5d740c4569f55d37a4151a49f4a0
KinzieK <120998570+KinzieK@users.noreply.github.com>
2023-02-27 21:52:39 +0000

    added rest of funtions

--- main.py
+++ main.py
@@ -1,57 +1,85 @@
 def on_button_pressed_a():
     global hours
+    basic.clear_screen()
     if hours < 23:
+        basic.clear_screen()
         hours += 1
-        basic.show_string("" + str((hours)))
+        basic.show_string("" + str(hours))
     else:
+        basic.clear_screen()
         hours = 0
-        basic.show_string("" + str((hours)))
+        basic.show_string("" + str(hours))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
     global time
     if minutes <= 9:
-        time = "" + str(hours) + (":" + "0" + str(minutes))
+        time = "" + str(hours) + (":" + "0" + ("" + str(minutes)))
         basic.clear_screen()
         basic.show_string(time)
     else:
-        time = "" + str(hours) + ":" + str(minutes)
+        time = "" + str(hours) + ":" + ("" + str(minutes))
+        basic.clear_screen()
+        basic.show_string(time)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global minutes
     if minutes < 59:
+        basic.clear_screen()
         minutes += 1
+        basic.show_string("" + str(minutes))
     else:
+        basic.clear_screen()
         minutes = 0
+        basic.show_string("" + str(minutes))
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 hours = 0
 minutes = 0
 time = ""
-basic.show_leds("""
-    . . . . .
-        . # # # .
-        . # # # .
-        . # # # .
-        . . . . .
-""")
+adjust = 0
+ampm = False
+ampm2 = False
+time2 = ""
+adjust2 = 0
+hours2 = 0
+minutes2 = 0
+time = ""
+minutes = 0
+hours = 0
 basic.pause(10)
 basic.show_leds("""
-    # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
+    # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
 """)
 basic.show_icon(IconNames.CHESSBOARD)
 basic.clear_screen()
-time = ""
-adjust = 0
-minutes = 0
-hours = 0
-ampm = False
 
 def on_forever():
-    pass
+    
+    def on_gesture_shake():
+        global adjust2, time2
+        adjust2 = hours2
+        if ampm2:
+            if hours2 > 12:
+                adjust2 = hours2 - 12
+            elif hours2 == 0:
+                adjust2 = 12
+        time2 = "" + ("" + str(adjust2))
+        time2 = time2 + ":"
+        if minutes2 < 10:
+            time2 = time2 + "0"
+        time2 = time2 + ("" + str(minutes2))
+        if ampm2:
+            if hours2 > 11:
+                time2 = time2 + "PM"
+            else:
+                time2 = time2 + "AM"
+        basic.show_string(time2)
+    input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+    
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -25,21 +25,26 @@ input.onButtonPressed(Button.B, function () {
     if (minutes < 59) {
         basic.clearScreen()
         minutes += 1
-        basic.showString("" + (minutes))
+        basic.showString("" + minutes)
     } else {
         basic.clearScreen()
         minutes = 0
-        basic.showString("" + (minutes))
+        basic.showString("" + minutes)
     }
 })
 let hours = 0
 let minutes = 0
 let time = ""
+let adjust = 0
+let ampm = false
+let ampm2 = false
+let time2 = ""
+let adjust2 = 0
+let hours2 = 0
+let minutes2 = 0
 time = ""
 minutes = 0
 hours = 0
-let ampm = false
-let adjust = 0
 basic.pause(10)
 basic.showLeds(`
     # . # . #
@@ -51,5 +56,15 @@ basic.showLeds(`
 basic.showIcon(IconNames.Chessboard)
 basic.clearScreen()
 basic.forever(function () {
-	
+    basic.pause(60000)
+    if (minutes < 59) {
+        minutes += 1
+    } else {
+        minutes = 0
+        if (hours < 23) {
+            hours += 1
+        } else {
+            hours = 0
+        }
+    }
 })
