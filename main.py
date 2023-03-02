f84cf1129c3a8c64eb7372d9aff9a9fd03ad55f3
KinzieK <120998570+KinzieK@users.noreply.github.com>
2023-03-02 14:51:58 +0000

    made compatible mbit v2

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
 def on_button_pressed_a():
     global hours
-    basic.clear_screen()
     if hours < 23:
-        basic.clear_screen()
         hours += 1
-        basic.show_string("" + str(hours))
-    else:
+        basic.show_string("" + str((hours)))
+        basic.pause(100)
         basic.clear_screen()
+    else:
+        basic.show_string("" + str((hours)))
         hours = 0
-        basic.show_string("" + str(hours))
+        basic.pause(100)
+        basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
     global time
     if minutes <= 9:
         time = "" + str(hours) + (":" + "0" + ("" + str(minutes)))
-        basic.clear_screen()
         basic.show_string(time)
     else:
         time = "" + str(hours) + ":" + ("" + str(minutes))
-        basic.clear_screen()
         basic.show_string(time)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global minutes
     if minutes < 59:
-        basic.clear_screen()
         minutes += 1
         basic.show_string("" + str(minutes))
     else:
-        basic.clear_screen()
         minutes = 0
         basic.show_string("" + str(minutes))
 input.on_button_pressed(Button.B, on_button_pressed_b)
@@ -38,48 +35,18 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 hours = 0
 minutes = 0
 time = ""
-adjust = 0
 ampm = False
-ampm2 = False
-time2 = ""
-adjust2 = 0
-hours2 = 0
-minutes2 = 0
+adjust = 0
 time = ""
 minutes = 0
 hours = 0
-basic.pause(10)
-basic.show_leds("""
-    # . # . #
-        . # . # .
-        # . # . #
-        . # . # .
-        # . # . #
-""")
-basic.show_icon(IconNames.CHESSBOARD)
-basic.clear_screen()
 
 def on_forever():
-    
-    def on_gesture_shake():
-        global adjust2, time2
-        adjust2 = hours2
-        if ampm2:
-            if hours2 > 12:
-                adjust2 = hours2 - 12
-            elif hours2 == 0:
-                adjust2 = 12
-        time2 = "" + ("" + str(adjust2))
-        time2 = time2 + ":"
-        if minutes2 < 10:
-            time2 = time2 + "0"
-        time2 = time2 + ("" + str(minutes2))
-        if ampm2:
-            if hours2 > 11:
-                time2 = time2 + "PM"
-            else:
-                time2 = time2 + "AM"
-        basic.show_string(time2)
-    input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-    
+    global minutes, hours
+    basic.pause(60000)
+    if minutes < 59:
+        minutes += 1
+    else:
+        minutes = 0
+        hours += 1
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,35 +1,32 @@
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
     if (hours < 23) {
-        basic.clearScreen()
         hours += 1
         basic.showString("" + hours)
-    } else {
+        basic.pause(100)
         basic.clearScreen()
-        hours = 0
+    } else {
         basic.showString("" + hours)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    if (minutes <= 9) {
-        time = "" + hours + (":" + "0" + ("" + minutes))
+        hours = 0
+        basic.pause(100)
         basic.clearScreen()
-        basic.showString(time)
-    } else {
-        time = "" + hours + ":" + ("" + minutes)
-        basic.clearScreen()
-        basic.showString(time)
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (minutes < 59) {
-        basic.clearScreen()
         minutes += 1
         basic.showString("" + minutes)
     } else {
-        basic.clearScreen()
         minutes = 0
         basic.showString("" + minutes)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (minutes <= 9) {
+        time = "" + hours + (":" + "0" + ("" + minutes))
+        basic.showString(time)
+    } else {
+        time = "" + hours + ":" + ("" + minutes)
+        basic.showString(time)
     }
 })
 let hours = 0
@@ -37,29 +34,15 @@ let minutes = 0
 let time = ""
 let adjust = 0
 let ampm = false
-let ampm2 = false
-let time2 = ""
-let adjust2 = 0
-let hours2 = 0
-let minutes2 = 0
 time = ""
 minutes = 0
 hours = 0
-basic.pause(10)
-basic.showLeds(`
-    # . # . #
-    . # . # .
-    # . # . #
-    . # . # .
-    # . # . #
-    `)
-basic.showIcon(IconNames.Chessboard)
-basic.clearScreen()
 basic.forever(function () {
     basic.pause(60000)
     if (minutes < 59) {
         minutes += 1
     } else {
         minutes = 0
+        hours += 1
     }
 })
